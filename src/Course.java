9960b65f7d0d3c80d5d83e208968936c9c806233
Sergei Bragin <mr.bragin@gmail.com>
2024-02-12 19:38:29 +0300

    Initial commit

---
+++ src/Course.java
@@ -0,0 +1,11 @@
+public class Course {
+    String title;
+    String discription;
+    int id;
+
+    public Course(String title, String discription, int id) {
+        this.title = title;
+        this.discription = discription;
+        this.id = id;
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,33 @@
+public class Student {
+    String name;
+    String surname;
+
+    int id;
+    int lastId = 0;
+
+    public Student(String name, String surname, int id) {
+        this.name = name;
+        this.surname = surname;
+        this.id = lastId++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
